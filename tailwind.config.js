fd65498ca6933751d4e37cf259371245371fd56e
Junior guinier <guinier95.js@gmail.com>
2024-11-21 16:30:26 +0100

    implementation du header

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,18 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        'manrope': ['Manrope', 'sans-serif'],  // Déclare la police Manrope
-        'anton': ['Anton', 'sans-serif'],     // Déclare la police Anton
+        manrope: ["Manrope", "sans-serif"], // Déclare la police Manrope
+        anton: ["Anton", "sans-serif"], // Déclare la police Anton
       },
+      colors: {
+        jaune: "#FFD15B",
+      },
+      fontSize: {
+        mainTitle: "44px",
+      },
+      center:{
+        
+      }
     },
   },
   plugins: [],
